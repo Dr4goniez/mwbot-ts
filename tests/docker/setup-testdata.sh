8007dce00f91ddf4f18b47777938beb260ea84e2
Dr4goniez <dragoniez.jawp@gmail.com>
2026-06-29 00:16:47 +0900

    test(setup-testdata): deduplicate code

--- tests/docker/setup-testdata.sh
+++ tests/docker/setup-testdata.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 
+create_page() {
+	local -r PAGE="$1"
+	php maintenance/run.php edit -a --createonly "$PAGE" <<< 'dummy content'
+}
+
 echo 'Creating an existing page for create() tests...'
-php maintenance/run.php edit -a --createonly Create_existing_page <<< 'dummy content'
+create_page Create_existing_page
 
 echo 'Creating an existing page for save() tests...'
-php maintenance/run.php edit -a --createonly Save_existing_page <<< 'dummy content'
+create_page Save_existing_page
 
 echo 'Creating an existing page for edit() tests...'
-php maintenance/run.php edit -a --createonly Edit_existing_page <<< 'dummy content'
+create_page Edit_existing_page
 
 echo 'Creating an existing page for newSection() tests...'
-php maintenance/run.php edit -a --createonly Talk:NewSection <<< 'dummy content'
+create_page Talk:NewSection
 
 echo 'Creating an existing page with an associated talk page for move() tests...'
-php maintenance/run.php edit -a --createonly Move_from <<< 'dummy content'
-php maintenance/run.php edit -a --createonly Talk:Move_from <<< 'dummy content'
+create_page Move_from
+create_page Talk:Move_from
 
 echo 'Creating two existing pages for read() tests...'
-php maintenance/run.php edit -a --createonly Read_1 <<< 'dummy content'
-php maintenance/run.php edit -a --createonly Read_2 <<< 'dummy content'
+create_page Read_1
+create_page Read_2
