e12742fb6b48a6267178f66315d4d0465ac2bd81
Dr4goniez <dragoniez.jawp@gmail.com>
2026-06-28 23:13:30 +0900

    test(mwbot): wrap integration tests for request() in describe()

--- tests/integration/MwbotTest-request-read.js
+++ tests/integration/MwbotTest-request-read.js
@@ -6,49 +6,51 @@ import { assert } from 'chai';
  */
 export function testMwbotReadRequests(getMwbot, _testDomain, _authMethod) {
 	describe('Mwbot read requests', function () {
-		it('should perform a GET request successfully', async function () {
-			const mwbot = getMwbot();
+		describe('request()', function () {
+			it('should perform a GET request successfully', async function () {
+				const mwbot = getMwbot();
 
-			const res = await mwbot.request(
-				{
-					action: 'query',
-					titles: 'Main Page',
-				},
-				{
-					method: 'GET',
-				}
-			);
-
-			assert.isObject(res.query);
-			assert.isArray(res.query?.pages);
-			assert.isObject(res.query?.pages?.[0]);
-			assert.isNumber(res.query?.pages?.[0].pageid);
-			assert.strictEqual(res.query?.pages?.[0].ns, 0);
-			assert.strictEqual(res.query?.pages?.[0].title, 'Main Page');
-		});
-
-		it('should perform a POST request successfully', async function () {
-			const mwbot = getMwbot();
-
-			const res = await mwbot.request(
-				{
-					action: 'query',
-					titles: 'Main Page',
-				},
-				{
-					method: 'POST',
-					headers: {
-						'Promise-Non-Write-API-Action': '1',
+				const res = await mwbot.request(
+					{
+						action: 'query',
+						titles: 'Main Page',
 					},
-				}
-			);
+					{
+						method: 'GET',
+					}
+				);
 
-			assert.isObject(res.query);
-			assert.isArray(res.query?.pages);
-			assert.isObject(res.query?.pages?.[0]);
-			assert.isNumber(res.query?.pages?.[0].pageid);
-			assert.strictEqual(res.query?.pages?.[0].ns, 0);
-			assert.strictEqual(res.query?.pages?.[0].title, 'Main Page');
+				assert.isObject(res.query);
+				assert.isArray(res.query?.pages);
+				assert.isObject(res.query?.pages?.[0]);
+				assert.isNumber(res.query?.pages?.[0].pageid);
+				assert.strictEqual(res.query?.pages?.[0].ns, 0);
+				assert.strictEqual(res.query?.pages?.[0].title, 'Main Page');
+			});
+
+			it('should perform a POST request successfully', async function () {
+				const mwbot = getMwbot();
+
+				const res = await mwbot.request(
+					{
+						action: 'query',
+						titles: 'Main Page',
+					},
+					{
+						method: 'POST',
+						headers: {
+							'Promise-Non-Write-API-Action': '1',
+						},
+					}
+				);
+
+				assert.isObject(res.query);
+				assert.isArray(res.query?.pages);
+				assert.isObject(res.query?.pages?.[0]);
+				assert.isNumber(res.query?.pages?.[0].pageid);
+				assert.strictEqual(res.query?.pages?.[0].ns, 0);
+				assert.strictEqual(res.query?.pages?.[0].title, 'Main Page');
+			});
 		});
 	});
 }
