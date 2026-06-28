5e2b5741b60be49b8046f90fdad45db126563b3e
Dr4goniez <dragoniez.jawp@gmail.com>
2026-06-29 00:03:17 +0900

    test(mwbot): add integration tests for read()

--- tests/docker/setup-testdata.sh
+++ tests/docker/setup-testdata.sh
@@ -16,3 +16,7 @@ php maintenance/run.php edit -a --createonly Talk:NewSection <<< 'dummy content'
 echo 'Creating an existing page with an associated talk page for move() tests...'
 php maintenance/run.php edit -a --createonly Move_from <<< 'dummy content'
 php maintenance/run.php edit -a --createonly Talk:Move_from <<< 'dummy content'
+
+echo 'Creating two existing pages for read() tests...'
+php maintenance/run.php edit -a --createonly Read_1 <<< 'dummy content'
+php maintenance/run.php edit -a --createonly Read_2 <<< 'dummy content'
--- tests/integration/MwbotTest-request-read.js
+++ tests/integration/MwbotTest-request-read.js
@@ -1,10 +1,11 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
+import { MwbotError } from '../../dist/index.js';
 
 /**
  * @type {import('./provider-types.js').MwbotTestSuite}
  */
-export function testMwbotReadRequests(getMwbot, _testDomain, _authMethod) {
+export function testMwbotReadRequests(getMwbot, testDomain, _authMethod) {
 	describe('Mwbot read requests', function () {
 		describe('request()', function () {
 			it('should perform a GET request successfully', async function () {
@@ -50,6 +51,131 @@ export function testMwbotReadRequests(getMwbot, _testDomain, _authMethod) {
 				assert.isNumber(res.query?.pages?.[0].pageid);
 				assert.strictEqual(res.query?.pages?.[0].ns, 0);
 				assert.strictEqual(res.query?.pages?.[0].title, 'Main Page');
+			});
+		});
+
+		describe('read()', function () {
+
+			/**
+			 * @param {1 | 2 | 3} index `3` for a non-existing page
+			 * @returns {{ ns: number; title: string; }}
+			 */
+			const getTitle = (index) => {
+				let title = `Read ${index}`;
+				let ns = 0;
+				if (testDomain === 'testwiki') {
+					title = 'User:Dragoniez/CI/' + title;
+					ns = 2;
+				}
+				return { ns, title };
+			};
+
+			/**
+			 * @param {import('../../dist/index.js').Revision} res
+			 * @param {number} ns
+			 * @param {string} title
+			 */
+			const validateReadResponseObject = (res, ns, title) => {
+				/* eslint-disable @typescript-eslint/no-unused-vars */
+				const {
+					pageid,
+					ns: _ns,
+					title: _title,
+					baserevid,
+					user,
+					basetimestamp,
+					starttimestamp,
+					content,
+					...rest
+				} = res;
+				/* eslint-enable @typescript-eslint/no-unused-vars */
+
+				assert.deepInclude(
+					res,
+					{
+						// pageid,
+						ns,
+						title,
+						// baserevid,
+						// user,
+						// basetimestamp,
+						// starttimestamp,
+						content: 'dummy content',
+					}
+				);
+
+				assert.isAbove(pageid, 0);
+				assert.isAbove(baserevid, 0);
+				assert.isString(user);
+				assert.isString(basetimestamp);
+				assert.isString(starttimestamp);
+
+				assert.isEmpty(rest);
+			};
+
+			it('should read a single page', async function () {
+				const { ns, title } = getTitle(1);
+
+				const res = await getMwbot().read(title);
+
+				validateReadResponseObject(res, ns, title);
+			});
+
+			it('should read multiple pages', async function () {
+				const titles = [
+					getTitle(1),
+					getTitle(2),
+				];
+
+				const res = await getMwbot().read(titles.map(obj => obj.title));
+
+				assert.notInstanceOf(res[0], MwbotError);
+				validateReadResponseObject(res[0], titles[0].ns, titles[0].title);
+
+				assert.notInstanceOf(res[1], MwbotError);
+				validateReadResponseObject(res[1], titles[1].ns, titles[1].title);
+			});
+
+			it('should throw for a missing page when reading a single title', async function () {
+				const { title } = getTitle(3);
+
+				try {
+					await getMwbot().read(title);
+					assert.fail('Expected read() to throw');
+				} catch (err) {
+					assert.instanceOf(err, MwbotError);
+					assert.strictEqual(err.code, 'pagemissing');
+				}
+			});
+
+			it('should return errors for missing pages when reading multiple titles', async function () {
+				const titles = [
+					getTitle(1),
+					getTitle(3),
+				];
+
+				const res = await getMwbot().read(titles.map(obj => obj.title));
+
+				assert.notInstanceOf(res[0], MwbotError);
+				validateReadResponseObject(res[0], titles[0].ns, titles[0].title);
+
+				assert.instanceOf(res[1], MwbotError);
+				assert.strictEqual(res[1].code, 'pagemissing');
+			});
+
+			it('should preserve duplicate titles', async function () {
+				const { ns, title } = getTitle(1);
+
+				const res = await getMwbot().read([title, title]);
+
+				assert.notInstanceOf(res[0], MwbotError);
+				validateReadResponseObject(res[0], ns, title);
+
+				assert.notInstanceOf(res[1], MwbotError);
+				validateReadResponseObject(res[1], ns, title);
+
+				assert.deepEqual(res[0], res[1]);
+				assert.notStrictEqual(res[0], res[1]);
 			});
 		});
 	});
