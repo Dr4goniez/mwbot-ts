8543516f5e0cf5129020bd1a9b8d018325d7f867
Dr4goniez <dragoniez.jawp@gmail.com>
2026-07-09 00:25:25 +0900

    test(wikitext): add unit tests for parseTags()

--- tests/unit/WikitextTest/WikitextTest-tags.js
+++ tests/unit/WikitextTest/WikitextTest-tags.js
@@ -70,5 +70,266 @@ export function testWikitextTags() {
 				);
 			});
 		});
+
+		describe('parseTags()', function () {
+			it('should return an empty array when no tags exist', function () {
+				const wkt = new mwbot.Wikitext('Foo');
+
+				assert.deepEqual(wkt.parseTags(), []);
+			});
+
+			it('should parse a normal HTML tag', function () {
+				const wkt = new mwbot.Wikitext('<div>Foo</div>');
+				const tags = wkt.parseTags();
+
+				assert.lengthOf(tags, 1);
+
+				assert.include(tags[0], {
+					name: 'div',
+					start: '<div>',
+					content: 'Foo',
+					end: '</div>',
+					text: '<div>Foo</div>',
+					void: false,
+					unclosed: false,
+					selfClosing: false,
+					skip: false,
+					nestLevel: 0,
+					index: 0,
+				});
+			});
+
+			it('should normalize tag names to lowercase', function () {
+				const wkt = new mwbot.Wikitext('<DIV>Foo</DIV>');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.name, 'div');
+			});
+
+			it('should parse nested tags', function () {
+				const wkt = new mwbot.Wikitext('<div><span>Foo</span></div>');
+				const tags = wkt.parseTags();
+
+				assert.lengthOf(tags, 2);
+
+				assert.strictEqual(tags[0].name, 'div');
+				assert.strictEqual(tags[1].name, 'span');
+
+				assert.strictEqual(tags[0].index, 0);
+				assert.strictEqual(tags[1].index, 1);
+
+				assert.strictEqual(tags[0].nestLevel, 0);
+				assert.strictEqual(tags[1].nestLevel, 1);
+
+				assert.isNull(tags[0].parent);
+				assert.strictEqual(tags[1].parent, 0);
+
+				assert.deepEqual([...tags[0].children], [1]);
+				assert.deepEqual([...tags[1].children], []);
+			});
+
+			it('should supplement an end tag for an unclosed tag', function () {
+				const wkt = new mwbot.Wikitext('<div>Foo');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.name, 'div');
+				assert.isTrue(tag.unclosed);
+				assert.strictEqual(tag.end, '</div>');
+				assert.strictEqual(tag.text, '<div>Foo');
+			});
+
+			it('should supplement end tags for implicitly closed nested tags', function () {
+				const wkt = new mwbot.Wikitext('<div><span>Foo</div>');
+				const tags = wkt.parseTags();
+
+				assert.lengthOf(tags, 2);
+
+				assert.strictEqual(tags[0].name, 'div');
+				assert.isFalse(tags[0].unclosed);
+
+				assert.strictEqual(tags[1].name, 'span');
+				assert.isTrue(tags[1].unclosed);
+				assert.strictEqual(tags[1].end, '</span>');
+			});
+
+			it('should ignore unmatched end tags', function () {
+				const wkt = new mwbot.Wikitext('Foo</div>');
+				const tags = wkt.parseTags();
+
+				assert.deepEqual(tags, []);
+			});
+
+			it('should parse HTML comments', function () {
+				const wkt = new mwbot.Wikitext('<!--Foo-->');
+				const [tag] = wkt.parseTags();
+
+				assert.include(tag, {
+					name: '!--',
+					start: '<!--',
+					content: 'Foo',
+					end: '-->',
+					void: false,
+					unclosed: false,
+				});
+			});
+
+			it('should supplement an end marker for an unclosed HTML comment', function () {
+				const wkt = new mwbot.Wikitext('<!--Foo');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.name, '!--');
+				assert.isTrue(tag.unclosed);
+				assert.strictEqual(tag.end, '-->');
+				assert.strictEqual(tag.text, '<!--Foo');
+			});
+
+			it('should recognize void tags', function () {
+				const wkt = new mwbot.Wikitext('Foo<br>Bar');
+				const [tag] = wkt.parseTags();
+
+				assert.include(tag, {
+					name: 'br',
+					void: true,
+					unclosed: false,
+					selfClosing: false,
+				});
+			});
+
+			it('should recognize self-closing tags', function () {
+				const wkt = new mwbot.Wikitext('<br />');
+				const [tag] = wkt.parseTags();
+
+				assert.isTrue(tag.void);
+				assert.isTrue(tag.selfClosing);
+			});
+
+			it('should convert </br> into a void tag', function () {
+				const wkt = new mwbot.Wikitext('</br>');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.name, 'br');
+				assert.isTrue(tag.void);
+				assert.isFalse(tag.selfClosing);
+			});
+
+			it('should treat a self-closing non-void tag as unclosed', function () {
+				const wkt = new mwbot.Wikitext('<span />Foo');
+				const [tag] = wkt.parseTags();
+
+				assert.include(tag, {
+					name: 'span',
+					selfClosing: true,
+					unclosed: true,
+					void: false,
+					content: 'Foo',
+					end: '</span>',
+					text: '<span />Foo',
+				});
+			});
+
+			it('should recognize parser extension tags', function () {
+				const wkt = new mwbot.Wikitext('<nowiki>Foo</nowiki>');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.name, 'nowiki');
+				assert.isFalse(tag.void);
+				assert.isFalse(tag.unclosed);
+			});
+
+			it('should treat a self-closing parser extension tag as unclosed', function () {
+				const wkt = new mwbot.Wikitext('<nowiki />Foo');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.name, 'nowiki');
+				assert.isTrue(tag.selfClosing);
+				assert.isTrue(tag.unclosed);
+			});
+
+			it('should mark tags inside skip tags', function () {
+				const wkt = new mwbot.Wikitext(
+					'<nowiki><div>Foo</div></nowiki>'
+				);
+
+				const tags = wkt.parseTags();
+
+				assert.isFalse(tags[0].skip);
+				assert.isTrue(tags[1].skip);
+			});
+
+			it('should record tag positions', function () {
+				const wkt = new mwbot.Wikitext('abc<div>Foo</div>xyz');
+				const [tag] = wkt.parseTags();
+
+				assert.strictEqual(tag.startIndex, 3);
+				assert.strictEqual(tag.endIndex, 17);
+			});
+
+			it('should assign children', function () {
+				const wkt = new mwbot.Wikitext(
+					'<div><span></span><b></b></div>'
+				);
+
+				const tags = wkt.parseTags();
+
+				assert.deepEqual([...tags[0].children], [1, 2]);
+			});
+
+			it('should ignore an extra closing tag', function () {
+				const wkt = new mwbot.Wikitext(
+					'<div></div></div>'
+				);
+
+				const tags = wkt.parseTags();
+
+				assert.lengthOf(tags, 1);
+			});
+
+			it('should return tags in source order', function () {
+				const wkt = new mwbot.Wikitext(
+					'<div><span></div>'
+				);
+
+				const tags = wkt.parseTags();
+
+				assert.deepEqual(
+					tags.map(({ name }) => name),
+					['div', 'span']
+				);
+			});
+
+			it('should filter by namePredicate', function () {
+				const wkt = new mwbot.Wikitext('<div><span>Foo</span></div>');
+
+				const tags = wkt.parseTags({
+					namePredicate: (name) => name === 'span',
+				});
+
+				assert.lengthOf(tags, 1);
+				assert.strictEqual(tags[0].name, 'span');
+			});
+
+			it('should filter by tagPredicate', function () {
+				const wkt = new mwbot.Wikitext('<div><span>Foo</span></div>');
+
+				const tags = wkt.parseTags({
+					tagPredicate: (tag) => tag.nestLevel === 0,
+				});
+
+				assert.lengthOf(tags, 1);
+				assert.strictEqual(tags[0].name, 'div');
+			});
+
+			it('should apply both predicates', function () {
+				const wkt = new mwbot.Wikitext('<div><span>Foo</span></div>');
+
+				const tags = wkt.parseTags({
+					namePredicate: (name) => name === 'span',
+					tagPredicate: (tag) => tag.nestLevel === 1,
+				});
+
+				assert.lengthOf(tags, 1);
+				assert.strictEqual(tags[0].name, 'span');
+			});
+		});
 	});
 }
